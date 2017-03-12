cc616cfbc64a4f9cdbebdad20115eb38a3eab4a1
saxenapragya8 <saxena.pragya8@gmail.com>
2017-03-12 17:30:48 -0400

    fixing comment activity launch without errors

--- app/src/main/java/wayt/com/whatareyourthoughts/ShowConversationCommentsActivity.java
+++ app/src/main/java/wayt/com/whatareyourthoughts/ShowConversationCommentsActivity.java
@@ -42,15 +42,6 @@ public class ShowConversationCommentsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         data = (ConversationsData) getIntent().getSerializableExtra("commentData");
 //        TextView participantList = (TextView)findViewById(R.id.participants);
 //        participantList.setText(data.getParticipantUsers().toString());
@@ -74,9 +65,6 @@ public class ShowConversationCommentsActivity extends AppCompatActivity {
         String newCommentText = comment.getText().toString();
         String convId = data.getConvId();
         RealtimeDbWriter.getInstance(this).addNewComment(convId, newCommentText);
-//        Integer convId = data.
-//        Integer userId = getUserId();
-//        HttpRequestSender.getInstance(this).addNewComment(userId, convId, this, newCommentText);
     }
 
     private Integer getUserId(){
